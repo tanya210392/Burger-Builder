79abb8f12c2d2155886efa3dd3a587c3affadf5f
tatiana_abchynets <tatiana.abchynets@headworks.com.ua>
2019-06-11 09:40:17 +0300

    redux devtools sttings, prepare folder structure

--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import reducer from './store/reducer';
+import burgerBuilder from './store/reducers/burgerBuilder';
 
-const store = createStore(reducer);
+const store = createStore(
+    burgerBuilder,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
     <Provider store={store}>
